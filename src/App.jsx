715bd0a3ac6ab78d948d3a6a92e8a87677c0aa08
SuranSandeepa <sandeepa.uththamawadu@gmail.com>
2023-05-09 16:02:18 +0530

    Created the  component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
+import React, { useState } from "react";
 
 function App() {
-
   return (
-    <>
-      <div>
-
-      </div>
-    </>
-  )
+    <div className="app">
+      <TodoList />
+    </div>
+  );
 }
 
-export default App
+
+function TodoList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleAddTask = () => {
+    // Logic for adding a new task to the list
+  };
+
+  const handleDeleteTask = (index) => {
+    // Logic for deleting a task from the list
+  };
+
+  return (
+    <div className="todo-list">
+      <h2>Todo List</h2>
+      {/* Input field for adding new tasks */}
+      {/* List of tasks */}
+      {/* Buttons for marking tasks as completed or deleting them */}
+    </div>
+  );
+}
+
+export default App;
